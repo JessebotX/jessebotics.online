aab0e1415e769f9244953180169a9175948fd40f
Jesse <jessebot.git@gmail.com>
2023-09-11 11:43:03 -0700

    reinit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-gradient-to-r from-purple-500 to-violet-900 mx-auto max-w-7xl">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/posts/hello-world.md
+++
@@ -1,9 +0,0 @@
----
-title: Hello, world!
-author: Jesse Huang
-date: "2023-02-22T00:00:00-07:00"
----
-
-Hello, world! This is my first blog post!
-
-# Now Goodbye
